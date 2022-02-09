162dd0edc4ded21f3cb316d92f45a8e11ad64cd3
cwyoon96 <chq1014@gmail.com>
2022-02-09 11:20:05 +0900

    posts

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "PRML 스터디 Chap.2 (1)",
+        "excerpt":"(본 내용은 독자가 학부 확률론 및 수리통계학 지식이 있다는 가정 하에 작성되었습니다.) PRML의 Chap.2 에서는 통계학에서 그리고 머신러닝에서 많이 쓰이는 여러 확률분포에 대한 내용을 다루고 있다. 2.1 Binary Variables 먼저, Binary Variable을 설명하기 위한 확률 분포들이다. Binary Random Variable이란 결과가 0 혹은 1 과 같이 둘 중 하나로 정해지는 Variable을...","categories": ["PRML 스터디"],
+        "tags": ["Machine Learning","PRML"],
+        "url": "/prml%20%EC%8A%A4%ED%84%B0%EB%94%94/prml-%EC%8A%A4%ED%84%B0%EB%94%94-chap-2-1/",
+        "teaser": null
+      }]
